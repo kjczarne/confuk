d415e24e1e5e118bb4d86b08451e4d9c40714120
Chris Czarnecki <kjczarne@gmail.com>
2024-09-28 14:01:21 -0400

    Variable interpolation and `omegaconf` support

--- confuk/parse.py
+++ confuk/parse.py
@@ -3,25 +3,51 @@ from typing import Type, Any, Literal, Dict, List
 from pydantic import BaseModel
 from pathlib import Path
 from easydict import EasyDict as edict
+from omegaconf import OmegaConf, DictConfig as OmegaConfigDict
 
 CfgClass = Type[Any]
 PydanticCfgClass = Type[BaseModel]
+ConfigDict = Dict[str, Any]
+SupportedCfgLiterals = Literal[
+    "dict", "d"
+    "attr", "edict", "ed"
+    "omega", "omegaconf", "o"
+]
 
 
-def _handle_import_path(toml_file_path: Path, import_path: Path | str) -> Path:
+def _build_repl_dict(toml_file_path: Path) -> Dict[str, Any]:
+    """Builds a dict of variable replacements that are built into the config loader.
+    Two formats are supported: `$something` (older) and `${something}` (newer, recommended).
+    """
     repls = {
         "$this_file": toml_file_path,
         "$this_dir": toml_file_path.parent,
         "$cwd": Path.cwd()
     }
-    for key, value in repls.items():
-        import_path = Path(str(import_path).replace(key, str(value)))
+    repls_with_curly_braces = {"${" + f"{k.replace('$', '')}" + "}": v for k, v in repls.items()}
+    repls.update(repls_with_curly_braces)
+    return repls
+
+
+def _variable_interpolation(ipt: str, key: str, repl_dict: Dict[str, Any]):
+    """Performs a basic variable interpolation by replacing the key with a value
+    picked out from a provided dictionary.
+    """
+    if key in repl_dict.keys():
+        return ipt.replace(key, str(repl_dict[key]))
+    raise KeyError(f"{key} not found in the dictionary provided. Keys that exist: {tuple(repl_dict.keys())}")
+
+
+def _handle_import_path(toml_file_path: Path, import_path: Path | str) -> Path:
+    repls = _build_repl_dict(toml_file_path)
+    for key in repls.keys():
+        import_path = _variable_interpolation(import_path, key, repls)
     if import_path == toml_file_path:
         raise ValueError("Import path cannot be the same as the current file")
     return import_path
 
 
-def _handle_imports(imports_list: List[Path]) -> Dict[str, Any]:
+def _handle_imports(imports_list: List[Path]) -> ConfigDict:
     out = {}
     for import_ in imports_list:
         import_dict = _parse_config_dict(import_)
@@ -29,7 +55,7 @@ def _handle_imports(imports_list: List[Path]) -> Dict[str, Any]:
     return out
 
 
-def _recursive_dict_update(d: Dict[str, Any], u: Dict[str, Any]) -> Dict[str, Any]:
+def _recursive_dict_update(d: ConfigDict, u: ConfigDict) -> ConfigDict:
     for k, v in u.items():
         if isinstance(v, dict):
             d[k] = _recursive_dict_update(d.get(k, {}), v)
@@ -38,7 +64,7 @@ def _recursive_dict_update(d: Dict[str, Any], u: Dict[str, Any]) -> Dict[str, An
     return d
 
 
-def _handle_preamble(config_dict: Dict[str, Any], toml_file_path: Path) -> Dict[str, Any]:
+def _handle_preamble(config_dict: ConfigDict, toml_file_path: Path) -> ConfigDict:
     if "pre" in config_dict.keys():
         pre = config_dict["pre"]
         if "imports" in pre.keys():
@@ -55,36 +81,52 @@ def _handle_preamble(config_dict: Dict[str, Any], toml_file_path: Path) -> Dict[
     return config_dict
 
 
-def _remove_preamble(config_dict: Dict[str, Any]) -> Dict[str, Any]:
+def _remove_preamble(config_dict: ConfigDict) -> ConfigDict:
     if "pre" in config_dict.keys():
         del config_dict["pre"]
     return config_dict
 
 
-def _parse_config_dict(toml_file: Path):
+def _handle_variable_interpolation(config_dict: ConfigDict):
+    # Lazy, but we borrow this from `omegaconf`, which is
+    # the most brilliant package for configuration and
+    # we support it as an output, so might as well use
+    # existing solutions to old problems:
+    config_odict = OmegaConfigDict(config_dict)
+    config = OmegaConf.create(config_odict)
+    return OmegaConf.to_container(config, resolve=True)
+
+
+def _parse_config_dict(toml_file: Path) -> ConfigDict:
     with open(toml_file, 'r') as file:
         config_dict = toml.load(file)
         config_dict = _handle_preamble(config_dict, toml_file)
         config_dict = _remove_preamble(config_dict)
+        config_dict = _handle_variable_interpolation(config_dict)
         return config_dict
 
 
-def _parse_config_kwarg_constructor(toml_file: Path, cfg_class: CfgClass):
+def _parse_config_kwarg_constructor(toml_file: Path, cfg_class: CfgClass) -> CfgClass:
     config_dict = _parse_config_dict(toml_file)
     config = cfg_class(**config_dict)
     return config
 
 
-def _parse_config_pydantic(toml_file: Path, cfg_class: PydanticCfgClass):
+def _parse_config_pydantic(toml_file: Path, cfg_class: PydanticCfgClass) -> BaseModel:
     return _parse_config_kwarg_constructor(toml_file, cfg_class)
 
 
-def _parse_config_easydict(toml_file: Path):
+def _parse_config_easydict(toml_file: Path) -> edict:
     config_dict = _parse_config_dict(toml_file)
     return edict(config_dict)
 
 
-def parse_config(toml_file: Path, cfg_class: CfgClass | Literal["attr"] | None = None):
+def _parse_omegaconfig(toml_file: Path) -> OmegaConf:
+    config_dict = _parse_config_dict(toml_file)
+    return OmegaConf.create(config_dict)
+
+
+def parse_config(toml_file: Path, cfg_class: CfgClass | SupportedCfgLiterals | None = None):
     """Takes a path object to a toml file and returns a config object.
 
     Args:
@@ -97,10 +139,12 @@ def parse_config(toml_file: Path, cfg_class: CfgClass | Literal["attr"] | None =
         An instance of the class used to load the config
     """
     match cfg_class:
-        case None:
+        case None | "dict" | "d":
             return _parse_config_dict(toml_file)
-        case "attr":
+        case "attr" | "edict" | "ed":
             return _parse_config_easydict(toml_file)
+        case "omega" | "omegaconf" | "o":
+            return _parse_omegaconfig(toml_file)
         case BaseModel():
             return _parse_config_pydantic(toml_file, cfg_class)
         case _:
--- test/test_read.py
+++ test/test_read.py
@@ -1,6 +1,9 @@
 import unittest
 from confuk import parse_config
 from pathlib import Path
+from typing import *
+from easydict import EasyDict as edict
+from omegaconf import DictConfig as OmegaConfigDict
 
 
 class TestRead(unittest.TestCase):
@@ -10,10 +13,44 @@ class TestRead(unittest.TestCase):
         cls.path = Path(__file__).parent / "test.toml"
         cls.dct = {'my': {'mother': 1}, 'your': {'dad': {'father': 1}}}
 
+    def _make_test_configs(self, args: Tuple[str]):
+        configs = []
+        for arg in args:
+            cfg = parse_config(self.path, arg)
+            configs.append(cfg)
+        return configs
+
+    def _make_test_type(self, cfg, type_: Type):
+        self.assertIsInstance(cfg, type_)
+
+    def _make_test_attr_dot_access(self, cfg):
+        self.assertEqual(cfg.my.mother, 1)
+
+    def _make_test_dict_equality(self, cfg):
+        self.assertDictEqual(self.dct, dict(cfg))
+
+    def _make_test_object_equality(self, cfg):
+        self.assertEqual(self.dct, cfg)
+
+    def test_read_dict(self):
+        args = (None, "dict", "d")
+        for cfg in self._make_test_configs(args):
+            self._make_test_type(cfg, dict)
+            self._make_test_dict_equality(cfg)
+
     def test_read_edict(self):
-        ed = parse_config(self.path, "attr")
-        self.assertDictEqual(self.dct, ed)
-        self.assertEqual(ed.my.mother, 1)
+        args = ("attr", "ed", "edict")
+        for cfg in self._make_test_configs(args):
+            self._make_test_type(cfg, edict)
+            self._make_test_dict_equality(cfg)
+            self._make_test_attr_dot_access(cfg)
+
+    def test_read_omegaconf(self):
+        args = ("omegaconf", "o", "omega")
+        for cfg in self._make_test_configs(args):
+            self._make_test_type(cfg, OmegaConfigDict)
+            self._make_test_attr_dot_access(cfg)
+            self._make_test_object_equality(cfg)
 
 
 if __name__ == '__main__':
