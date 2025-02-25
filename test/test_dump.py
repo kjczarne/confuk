841af8f2601ba4d1137f3f2f1cf7a1b18ea333cd
Chris Czarnecki <kjczarne@gmail.com>
2025-02-25 12:16:24 -0500

    Fix: OmegaConf container passthrough

--- test/test_dump.py
+++ test/test_dump.py
@@ -1,6 +1,7 @@
 from confuk import dump_config
 import unittest
 from dataclasses import dataclass, field
+from omegaconf import DictConfig
 
 @dataclass
 class DummyCfg:
@@ -14,6 +15,7 @@ class TestDumpConfig(unittest.TestCase):
     def setUpClass(cls):
         cls.config = DummyCfg(a=10, b="my momma")
         cls.config_dict = cls.config.__dict__
+        cls.omegaconf = DictConfig(cls.config_dict)
 
     def test_dump_config(self):
         dump_config(self.config_dict, "test/outputs/dump.json")
@@ -21,6 +23,9 @@ class TestDumpConfig(unittest.TestCase):
         dump_config(self.config_dict, "test/outputs/dump.yaml")
         dump_config(self.config, "test/outputs/dump.jsonp")
         dump_config(self.config, "test/outputs/dump.pkl")
+        dump_config(self.omegaconf, "test/outputs/dump_omegaconf.toml")
+        dump_config(self.omegaconf, "test/outputs/dump_omegaconf.yaml")
+        dump_config(self.omegaconf, "test/outputs/dump_omegaconf.json")
 
 
 if __name__ == "__main__":
